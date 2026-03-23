5645590bb178fbc9212d0a1839c80973cb6a7e31
Adrian Vargas <adrianfelipevr@gmail.com>
2026-03-22 21:06:00 -0500

    create game header stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { GameHeader } from "./components/GameHeader";
 
 function App() {
-  return <div className="app">
-    <GameHeader />
-  </div>;
+  return (
+    <div className="app">
+      <GameHeader score={3} moves={2} />
+    </div>
+  );
 }
 
 export default App;
